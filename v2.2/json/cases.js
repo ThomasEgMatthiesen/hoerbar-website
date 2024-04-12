da3697a1420731b9bf6a8d2c08f9b85cbdcd7c8b
Thomas Eg Matthiesen <kontakt@thomaseg.dk>
2024-04-12 10:15:59 +0200

    Create cases.js

---
+++ v2.2/json/cases.js
@@ -0,0 +1,52 @@
+[
+  {
+    "title": "Boligportal",
+    "subtitle": "Lyden af at finde et nyt hjem",
+    "services": "// Lydidentitet, Lydlogo, Brandmusik",
+    "theme": "Lydbranding",
+    "year": "// Lydbranding - 2023",
+    "link": "",
+    "imgUrl": "/media/images/foto_00.jpeg",
+    "intro": ""
+  },
+  {
+    "title": "Ildhu CPH",
+    "subtitle": "Nordisk børnetøj",
+    "services": "// Lydlogo, Brandmusik",
+    "theme": "Lydbranding",
+    "year": "// Lydbranding - 2022",
+    "link": "",
+    "imgUrl": "/media/images/foto_00.jpeg",
+    "content":""
+  },
+  {
+    "title": "Venstre",
+    "subtitle": "Politisk klangbillede",
+    "services": "// Lydlogo, Podcast",
+    "theme": "Lydbranding",
+    "year": "2022",
+    "link": "",
+    "imgUrl": "/media/images/foto_00.jpeg",
+    "content":""
+  },
+  {
+    "title": "Minddiver",
+    "subtitle": "Lyddesign til en rejse ind i sindet",
+    "services": "// Lyddesign, Implementation",
+    "theme": "Game audio",
+    "year": "2022",
+    "link": "",
+    "imgUrl": "/media/images/foto_00.jpeg",
+    "content":""
+  },
+  {
+    "title": "Windfall",
+    "subtitle": "Eksplorativ fortælling",
+    "services": "// Lyddesign, Implementation",
+    "theme": "Game audio",
+    "year": "2021",
+    "link": "",
+    "imgUrl": "/media/images/foto_00.jpeg",
+    "content":""
+  }
+]
